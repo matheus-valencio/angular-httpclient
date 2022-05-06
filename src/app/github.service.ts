f969aa91b0ece3e3929f6a240d17f9e0cffc35e1
matheus-valencio <matheus.valencio@live.com>
2022-05-06 19:22:55 -0300

    4 files modified, 1 added

---
+++ src/app/github.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+interface Repo {
+  id: number;
+  full_name: string;
+}
+
+@Injectable()
+export class GithubService {
+  repos: Array<Repo> = [];
+
+  constructor(private http: HttpClient) {}
+}
